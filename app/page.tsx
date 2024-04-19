05ed399c7a6265c94a9e27528f69778fa969e53f
Victor Akintunde <akintoluvic@gmail.com>
2024-04-19 23:21:12 +0100

    setup convex with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>
-    <p>Hello Oniro</p>
     <Button size="sm" variant="outline">Asksk</Button>
-    </>
   );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://champion-raven-2.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};

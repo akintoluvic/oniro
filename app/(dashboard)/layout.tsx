5f16c8c2c168cde6ac590916360e92932587ed2e
Victor Akintunde <akintoluvic@gmail.com>
2024-04-20 00:00:24 +0100

    sidebar init

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import { Sidebar } from "./_components/sidebar"
+
+interface DashboardLayoutProps {
+  children: React.ReactNode
+}
+
+const DashboardLayout = ({
+  children
+}: DashboardLayoutProps) => {
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          {/* Add Navbar */}
+          {children}
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default DashboardLayout

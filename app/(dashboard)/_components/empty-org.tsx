a0e138b35ea1f0b262ba0f7a1c19be00a3bbc02a
Victor Akintunde <akintoluvic@gmail.com>
2024-04-24 11:13:02 +0100

    fix no org empty state

---
+++ app/(dashboard)/_components/empty-org.tsx
@@ -0,0 +1,40 @@
+import { CreateOrganization } from "@clerk/nextjs"
+import Image from "next/image"
+import { 
+  Dialog,
+  DialogContent,
+  DialogTrigger
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+
+export const EmptyOrg = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image
+        src='/elements.svg'
+        alt="Empty"
+        height={200}
+        width={200}
+      />
+      <h2 className="text-2xl font-semibold mt-6">
+        Welcome to Board
+      </h2>
+      <p className="mt-2 text-sm text-muted-foreground">
+        Create an organization to get started
+      </p>
+      <div className="mt-6">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              Create organizatioon
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+            <CreateOrganization />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  )
+}

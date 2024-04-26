513a0ccb34a03de3e9642c94fad2e10c648240af
Victor Akintunde <akintoluvic@gmail.com>
2024-04-26 10:57:19 +0100

    fix card skeleton loader

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -24,7 +24,19 @@ export const BoardList = ({
 
   if (data === undefined) {
     return (
-      <div>Loading...</div>
+      <div>
+        <h2 className="text-3xl">
+          {query.favorites ? 'Favorite boards' : 'Team boards'}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
     )
   }
 
@@ -51,7 +63,7 @@ export const BoardList = ({
       <h2 className="text-3xl">
         {query.favorites ? 'Favorite boards' : 'Team boards'}
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt8 pb-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
         <NewBoardButton orgId={orgId}/>
         {data?.map(board => (
           <BoardCard

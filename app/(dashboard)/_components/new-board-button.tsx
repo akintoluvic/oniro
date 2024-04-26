f0cc50bc895aa818617b7bab0eb37075c7016346
Victor Akintunde <akintoluvic@gmail.com>
2024-04-26 09:50:29 +0100

    fix new board button with api

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { api } from "@/convex/_generated/api"
+import { useApiMutation } from "@/hooks/use-api-mutation"
 import { cn } from "@/lib/utils"
 import { Plus } from "lucide-react"
+import { toast } from "sonner"
 
 interface NewBoardButtonProps {
   orgId: string
@@ -11,13 +14,24 @@ interface NewBoardButtonProps {
 export const NewBoardButton = ({
   orgId, disabled
 }: NewBoardButtonProps) => {
+
+  const { mutate, pending } = useApiMutation(api.board.create)
+
+  const onClick = () => {
+    mutate({orgId, title: 'New untitled'})
+      .then(id => {
+        toast.success('New board created')
+        // TODO: Redirect to /board/(id)
+      })
+      .catch(() => toast.error('Failed to create board'))
+  }
   return (
     <button 
       disabled={disabled}
-      onClick={() => {}}
+      onClick={onClick}
       className={cn(
         'col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6',
-        disabled && 'opacity-75'
+        pending || disabled && 'opacity-75'
       )}
     >
       <div></div>

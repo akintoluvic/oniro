0a329332a0b955593e9c9324cfc070618e8541b2
Victor Akintunde <akintoluvic@gmail.com>
2024-04-29 14:40:22 +0100

    fix update board api

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
 import { mutation } from "./_generated/server";
+import { title } from "process";
 
 const images = [
   '/placeholders/1.svg',
@@ -53,5 +54,31 @@ export const remove = mutation({
     // TODO: check to delete favorite relation as well
 
     await ctx.db.delete(args.id)
+  }
+})
+
+export const update = mutation({
+  args: { id: v.id('boards'), title: v.string() },
+  handler: async (ctx, args) => {
+    const title = args.title.trim()
+    const identity = await ctx.auth.getUserIdentity()
+
+    if(!identity) {
+      throw new Error('User not authenticated.')
+    }
+
+    if (!title) {
+      throw new Error('Title is required')
+    }
+
+    if(title.length > 60) {
+      throw new Error('Title can not be more than 60 characters')
+    }
+
+    const board = await ctx.db.patch(args.id, {
+      title: args.title
+    })
+
+    return board
   }
 })

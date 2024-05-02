92a9703673a878732e0c294cc178ab784064a3bd
Victor Akintunde <akintoluvic@gmail.com>
2024-05-02 13:46:51 +0100

    init users list

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Info } from "./info"
+import { Participants } from "./participants"
 
 export const Canvas = () => {
   return (
@@ -8,6 +9,7 @@ export const Canvas = () => {
       className="h-full w-full relative bg-neutral-100 touch-none"
     >
       <Info />
+      <Participants />
     </main>
   )
 }

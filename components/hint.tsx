a84d99f8c562d340f32a4fda273667d9c55a416a
Victor Akintunde <akintoluvic@gmail.com>
2024-05-05 11:20:38 +0100

    fix hint side props as optional

--- components/hint.tsx
+++ components/hint.tsx
@@ -9,7 +9,7 @@ import {
 export interface HintProps {
   label: string;
   children: React.ReactNode;
-  side: "top" | "bottom" | "left" | "right";
+  side?: "top" | "bottom" | "left" | "right";
   align?: "start" | "center" | "end";
   sideOffset?: number;
   alignOffset?: number;

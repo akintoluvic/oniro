6bbd7216e811035f84f4c9bbb1595dbd13c0fb78
Victor Akintunde <akintoluvic@gmail.com>
2024-05-06 11:30:32 +0100

    fix update canvas mode types

--- types/canvas.ts
+++ types/canvas.ts
@@ -1,9 +1,33 @@
 
 export type CanvasState = 
   | {
-  mode: CanvasMode.None
-}
+    mode: CanvasMode.None
+  }
+  | {
+    mode: CanvasMode.Pressing
+  }
+  | {
+    mode: CanvasMode.SelectionNet
+  }
+  | {
+    mode: CanvasMode.Translating
+  }
+  | {
+    mode: CanvasMode.Inserting
+  }
+  | {
+    mode: CanvasMode.Resizing
+  }
+  | {
+    mode: CanvasMode.Pencil
+  }
 
 export enum CanvasMode {
-  None
+  None,
+  Pressing,
+  SelectionNet,
+  Translating,
+  Inserting,
+  Resizing,
+  Pencil
 }

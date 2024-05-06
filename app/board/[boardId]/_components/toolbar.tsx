6bb8540271207e4ec42228bc0a55060ce878d8fc
Victor Akintunde <akintoluvic@gmail.com>
2024-05-06 11:01:18 +0100

    init toolbar icons

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,5 @@
+import { Circle, MousePointer2, Pencil, Redo2, Square, StickyNote, Type, Undo2 } from "lucide-react"
+import { ToolButton } from "./tool-button"
 
 export const Toolbar = () => {
   return (
@@ -5,14 +7,56 @@ export const Toolbar = () => {
       className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4"
     >
       <div className="bg-white rounded-md px-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div>Pencil</div>
-        <div>Square</div>
-        <div>Circle</div>
-        <div>Ellipsis</div>
+        <ToolButton
+          label="Select" 
+          icon={MousePointer2} 
+          onClick={() => {}} 
+          isActive={false}
+        />
+        <ToolButton
+          label="Text" 
+          icon={Type} 
+          onClick={() => {}} 
+          isActive={false}
+        />
+        <ToolButton
+          label="Sticky Note" 
+          icon={StickyNote} 
+          onClick={() => {}} 
+          isActive={false}
+        />
+        <ToolButton
+          label="Rectangle" 
+          icon={Square} 
+          onClick={() => {}} 
+          isActive={false}
+        />
+        <ToolButton
+          label="Ellipse" 
+          icon={Circle} 
+          onClick={() => {}} 
+          isActive={false}
+        />
+        <ToolButton
+          label="Pencil" 
+          icon={Pencil} 
+          onClick={() => {}} 
+          isActive={false}
+        />
       </div>
       <div className="bg-white rounded-md px-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div>Undo</div>
-        <div>Redo</div>
+      <ToolButton
+          label="Undo" 
+          icon={Undo2} 
+          onClick={() => {}} 
+          isActive={false}
+        />
+        <ToolButton
+          label="Redo" 
+          icon={Redo2} 
+          onClick={() => {}} 
+          isActive={false}
+        />
       </div>
     </div>
   )

644d565614e9dda405bbceb21ddf1abd4bfda5d1
Victor Akintunde <akintoluvic@gmail.com>
2024-05-06 10:41:25 +0100

    fix participants avatars

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,18 +1,50 @@
+"use client";
+
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { UserAvatar } from "./user-avatar";
+
+const MAX_SHOWN_OTHER_USERS = 1;
 
 export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_OTHER_USERS;
+
   return (
-    <div
-      className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md"
-    >
-      User lists
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_OTHER_USERS).map(({ connectionId, info }) => {
+          return (
+            <UserAvatar
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || 'T'}
+            />
+          );
+        })}
+        
+        {currentUser && (
+          <UserAvatar
+            src={currentUser.info?.picture}
+            name={currentUser.info?.name}
+            fallback={currentUser.info?.name?.[0] || 'T'}
+          />
+        )}
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`+${users.length - MAX_SHOWN_OTHER_USERS} more`}
+            fallback={`+${users.length - MAX_SHOWN_OTHER_USERS}`}
+          />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export const ParticipantsSkeleton = () => {
   return (
-    <div
-      className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]"
-    />
-  )
-}
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />
+  );
+};
